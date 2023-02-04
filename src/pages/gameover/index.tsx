6fc1680983dccb2e9b2c0fba575ec905ecf85e67
Gulben Emiroglu <62151539+gulbenemiroglu@users.noreply.github.com>
2023-02-04 22:23:29 +0300

    gameover page added

--- src/pages/gameover/index.tsx
+++ src/pages/gameover/index.tsx
@@ -1,10 +1,45 @@
+import { Col, Row, Container } from 'react-bootstrap';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 const GameOver = () => {
-    return(
-        <div >
-            <h1>GAME OVER</h1>
-            <h4>Your Score : <span>90</span>  </h4>
-            
-            
+    return (
+        <div className='p-3 fullH' style={{ backgroundColor: 'black' }}>
+
+            <Row style={{
+                display:'flex',
+                justifyContent:'center'
+            }}>
+                <Col className='gameovergif' xs={12}>
+                </Col>
+                <Col xs={12}>
+                    <div className='writeEditing mb-5'>
+                        <h4>Score</h4>
+                        <h1>90</h1>
+                    </div>
+
+                </Col>
+                <Col xs={6}>
+                    <Form style={{display: "flex",  alignItems: "baseline", justifyContent:"center"}}>
+                        <Form.Group className="mb-3 white" controlId="formBasicEmail">
+                            {/* <Form.Label style={{color:"white"}}>Username :</Form.Label> */}
+                            <Form.Control style={{border: "1px solid #141619", borderRadius:"unset"}} type="email" placeholder='Enter username' />
+                            
+                        </Form.Group>
+
+                        <Button variant="danger" type="submit" style={{
+                            border: "1px solid #141619",
+                            borderRadius:"unset",
+                            backgroundColor:"grey"
+                        }}>
+                            Save
+                        </Button>
+                    </Form>
+
+                </Col>
+            </Row>
+
         </div>
     )
 }
