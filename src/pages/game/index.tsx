96cd9e1bfa8ec6db5059e86de1e1d4626f049a6d
erdem-emiroglu <62151699+erdem-emiroglu@users.noreply.github.com>
2023-02-04 19:46:49 +0300

    server

--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -1,7 +1,11 @@
 import React, {useEffect, useState} from "react";
+const {useRouter} = require("next/router");
+
 
 const Game = () => {
+    const router = useRouter();
     const [score, setScore] = useState(0);
+    const [isGameOver, setIsGameOver] = useState(false);
     useEffect(() => {
         async function initPhaser() {
             const Phaser = await import("phaser");
@@ -19,12 +23,16 @@ const Game = () => {
                         this.bricks;
                         this.paddle;
                         this.ball;
+                        this.scoreText = 'asdqweqweqwe';
+                        this.score = 0;
                     },
 
                 preload: function ()
                 {
-                    this.load.image('cherry', 'assets/image/cherry.png', 20, 20);
-                    this.load.image('jar', 'assets/image/jar.png', 20, 20);
+                    this.load.image('cherry', 'assets/image/cherry.png');
+                    this.load.image('jar', 'assets/image/jar.png');
+
+                    this.scoreText = this.add.text(72, 72, 'score: 0', { fontSize: '32px', fill: '#000' });
                 },
 
                 create: function ()
@@ -39,10 +47,11 @@ const Game = () => {
                         gridAlign: { width: 10, height: 6, cellWidth: 128 + 30, cellHeight: 32 + 30, x: 240, y: 150 }
                     });
 
-                    this.ball = this.physics.add.sprite(400,500, 'cherry').setCollideWorldBounds(true).setScale(0.2).setBounce(1);
+                    this.ball = this.physics.add.sprite(400,800, 'cherry').setCollideWorldBounds(true).setScale(0.2).setBounce(1);
                     this.ball.setData('onPaddle', true);
 
-                    this.paddle = this.physics.add.image(400, 550, 'assets', 'paddle1').setImmovable();
+                    this.paddle = this.physics.add.image(400, 850, 'assets', 'paddle1').setImmovable();
+                    this.scoreText = this.add.text(72, 72, 'score: 0', { fontSize: '32px', fill: '#000' });
 
                     //  Our colliders
                     this.physics.add.collider(this.ball, this.bricks, this.hitBrick, null, this);
@@ -52,7 +61,7 @@ const Game = () => {
                     this.input.on('pointermove', function (pointer) {
 
                         //  Keep the paddle within the game
-                        this.paddle.x = Phaser.Math.Clamp(pointer.x, 52, 748);
+                        this.paddle.x = Phaser.Math.Clamp(pointer.x, 52, 1670);
 
                         if (this.ball.getData('onPaddle'))
                         {
@@ -74,8 +83,13 @@ const Game = () => {
 
                 hitBrick: function (ball, brick)
                 {
-                    brick.disableBody(false, false);
+                    brick.disableBody(true, true);
+                    this.score += 10;
+                    this.scoreText.setText('Score: ' + this.score);
 
+                    if(this.score === 100) {
+                        ball.setVelocity(500, 500);
+                    }
                     if (this.bricks.countActive() === 0)
                     {
                         this.resetLevel();
@@ -85,7 +99,7 @@ const Game = () => {
                 resetBall: function ()
                 {
                     this.ball.setVelocity(0);
-                    this.ball.setPosition(this.paddle.x, 500);
+                    this.ball.setPosition(this.paddle.x, 800);
                     this.ball.setData('onPaddle', true);
                 },
 
@@ -126,9 +140,9 @@ const Game = () => {
 
                 update: function ()
                 {
-                    if (this.ball.y > 600)
+                    if (this.ball.y > 900)
                     {
-                        this.resetBall();
+                        router.push('/game-over');
                     }
                 }
 
@@ -136,13 +150,18 @@ const Game = () => {
 
             var config = {
                 type: Phaser.AUTO,
-                width: 1920,
+                width: 1720,
                 height: 1080,
                 parent: "game-container",
                 title: "Pachinko",
                 scene: [ Pachinko ],
                 physics: {
                     default: 'arcade'
+                },
+                scale: {
+                    mode: Phaser.Scale.FIT,
+                    parent: 'phaser-example',
+                    autoCenter: Phaser.Scale.CENTER_BOTH,
                 }
             };
 
