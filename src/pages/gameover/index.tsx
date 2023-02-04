c30881a992ff12ab97d19b23dababcce34526943
Gulben Emiroglu <62151539+gulbenemiroglu@users.noreply.github.com>
2023-02-04 20:35:42 +0300

    4.02.023

---
+++ src/pages/gameover/index.tsx
@@ -0,0 +1,12 @@
+const GameOver = () => {
+    return(
+        <div >
+            <h1>GAME OVER</h1>
+            <h4>Your Score : <span>90</span>  </h4>
+            
+            
+        </div>
+    )
+}
+
+export default GameOver;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import {Button, Card} from "@mui/material";
 
 
+
+
 export default function Home() {
     return (
+        <>
         <Card style={{
             backgroundImage: `url("/assets/image/background.jpg")`,
             height: '100vh',
             width: '100vw'
         }}>
-            <img style={{position:"absolute", right: 0, height:100}} src="/assets/image/logo2.png" alt="_blank"/>
+            {/* <img style={{position:"absolute", right: 0, height:100}} src="/assets/image/logo2.png" alt="_blank"/> */}
             <div className="title" style={{
                 color: "#ffffff",
                 textAlign: "center",
@@ -17,14 +20,14 @@ export default function Home() {
                 fontSize: 100,
                 textShadow: 'rgb(136 136 136) -6px 0px 4px'
             }}>
-                PACHİNKO
+                PACHINKO
             </div>
-            <div style={{display: "flex", justifyContent: "center", marginTop: "8%"}}>
+            <div style={{display: "flex", justifyContent: "center", marginTop: "5%"}}>
                 <Button type='submit' href='/game'
                         style={{
                             textAlign: "center",
-                            width: 150,
-                            height: 150,
+                            width: 120,
+                            height: 120,
                             display: "flex",
                             backgroundColor: 'black',
                             opacity: 0.7,
@@ -33,5 +36,7 @@ export default function Home() {
                         }}>Play</Button>
             </div>
         </Card>
+       
+        </>
     );
 }
